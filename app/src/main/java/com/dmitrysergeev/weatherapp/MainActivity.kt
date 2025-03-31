e9cb8b9e9535fa8682194a92407b3a4b10c694a6
DmitrySergeev02 <justbringit16@yandex.ru>
2025-03-31 19:08:28 +0300

    Создана заготовка ViewModel для главного экрана

--- app/src/main/java/com/dmitrysergeev/weatherapp/MainActivity.kt
+++ app/src/main/java/com/dmitrysergeev/weatherapp/MainActivity.kt
@@ -2,11 +2,13 @@ package com.dmitrysergeev.weatherapp
 
 import android.os.Bundle
 import android.util.Log
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.dmitrysergeev.weatherapp.data.weather.WeatherRepository
 import com.dmitrysergeev.weatherapp.data.weather.api.WeatherApi
 import com.dmitrysergeev.weatherapp.data.weather.api.WeatherInterceptor
 import com.dmitrysergeev.weatherapp.data.weather.api.WeatherRepositoryApiImpl
+import com.dmitrysergeev.weatherapp.presentation.mainscreen.MainScreenViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,27 +24,17 @@ class MainActivity : AppCompatActivity() {
 
     @Inject lateinit var repo: WeatherRepository
 
+    private val mainScreenViewModel: MainScreenViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val client = OkHttpClient.Builder()
-//            .addInterceptor(WeatherInterceptor())
-//            .build()
-//
-//        val retrofit: Retrofit = Retrofit.Builder()
-//            .baseUrl("https://api.weatherapi.com/v1/")
-//            .addConverterFactory(MoshiConverterFactory.create())
-//            .client(client)
-//            .build()
-//
-//        val weatherApi: WeatherApi = retrofit.create<WeatherApi>()
-//
-//        val weatherRepository: WeatherRepository = WeatherRepositoryApiImpl(weatherApi)
-
         CoroutineScope(Dispatchers.IO).launch {
             Log.d(TAG, "${repo.getCurrentWeather("Moscow")}")
         }
+
+        mainScreenViewModel.test()
     }
 
     companion object {
--- app/src/main/java/com/dmitrysergeev/weatherapp/presentation/mainscreen/MainScreenViewModel.kt
+++ app/src/main/java/com/dmitrysergeev/weatherapp/presentation/mainscreen/MainScreenViewModel.kt
@@ -1,10 +1,30 @@
 package com.dmitrysergeev.weatherapp.presentation.mainscreen
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dmitrysergeev.weatherapp.data.weather.WeatherRepository
 import com.dmitrysergeev.weatherapp.data.weather.model.CurrentWeatherResponse
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class MainScreenViewModel: ViewModel() {
+@HiltViewModel
+class MainScreenViewModel @Inject constructor(
+    private val weatherRepository: WeatherRepository
+): ViewModel() {
 
-    private val currentWeatherResponse: CurrentWeatherResponse? = null
+    private var currentWeatherResponse: CurrentWeatherResponse? = null
+
+    init {
+        viewModelScope.launch {
+            currentWeatherResponse = weatherRepository.getCurrentWeather("Moscow")
+            Log.d("MainScreenViewModel", currentWeatherResponse.toString())
+        }
+    }
+
+    fun test(){
+        Log.d("MainScreenViewModel", "123")
+    }
 
 }
