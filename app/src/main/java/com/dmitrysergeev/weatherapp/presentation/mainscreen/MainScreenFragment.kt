dcd143d375de224d80e85f8704cff682c38b7200
DmitrySergeev02 <justbringit16@yandex.ru>
2025-04-03 14:42:20 +0300

    Hardcoded строки заменены на string resources

--- app/src/main/java/com/dmitrysergeev/weatherapp/presentation/mainscreen/MainScreenFragment.kt
+++ app/src/main/java/com/dmitrysergeev/weatherapp/presentation/mainscreen/MainScreenFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.dmitrysergeev.weatherapp.R
 import com.dmitrysergeev.weatherapp.databinding.FragmentMainScreenBinding
+import com.dmitrysergeev.weatherapp.databinding.WeatherCardItemBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -29,6 +31,17 @@ class MainScreenFragment: Fragment() {
 
         binding.testText.setOnClickListener {
             binding.testText.text = "Oops, Changed"
+        }
+
+        binding.card.apply {
+            rainChance.text = getString(R.string.chance_of_rain,60)
+            weatherStatus.text = "Partly Clody"
+            locationText.text = "Washington DC, USA"
+            precipitationText.text = getString(R.string.no_precipitation_for_at_least,120)
+            currentWeatherDegree.text = "26"
+            maxDegreeText.text = getString(R.string.values_celsius, 34)
+            minDegreeText.text = getString(R.string.values_celsius, 20)
+            feelsLikeText.text = getString(R.string.feels_like, 20)
         }
 
         viewModel.test()
