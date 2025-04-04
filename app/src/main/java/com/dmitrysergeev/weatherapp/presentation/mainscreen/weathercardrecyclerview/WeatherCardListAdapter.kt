a7f6e89d7848342b1b2d0e86c80accecde4ca823
DmitrySergeev02 <justbringit16@yandex.ru>
2025-04-04 22:56:38 +0300

    Добавлены классы для ViewHolder и Adapter

---
+++ app/src/main/java/com/dmitrysergeev/weatherapp/presentation/mainscreen/weathercardrecyclerview/WeatherCardListAdapter.kt
@@ -0,0 +1,44 @@
+package com.dmitrysergeev.weatherapp.presentation.mainscreen.weathercardrecyclerview
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.dmitrysergeev.weatherapp.R
+import com.dmitrysergeev.weatherapp.data.weather.model.ForecastWeatherResponse
+import com.dmitrysergeev.weatherapp.databinding.WeatherCardItemBinding
+
+class WeatherCardViewHolder(
+    private val binding: WeatherCardItemBinding
+): RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(forecastWeatherResponse: ForecastWeatherResponse){
+        binding.apply {
+            rainChance.text = itemView.context.getString(R.string.chance_of_rain, forecastWeatherResponse.forecast.forecastDay[0].dayApi.dailyChanceOfRain)
+            weatherStatus.text = forecastWeatherResponse.currentWeather.condition.text
+            locationText.text = itemView.context.getString(R.string.location, forecastWeatherResponse.location.name, forecastWeatherResponse.location.country)
+            precipitationText.text = itemView.context.getString(R.string.no_precipitation_for_at_least,120)
+            currentWeatherDegree.text = "${forecastWeatherResponse.currentWeather.tempC.toInt()}"
+            maxDegreeText.text = itemView.context.getString(R.string.values_celsius, forecastWeatherResponse.forecast.forecastDay[0].dayApi.maxTempC.toInt())
+            minDegreeText.text = itemView.context.getString(R.string.values_celsius, forecastWeatherResponse.forecast.forecastDay[0].dayApi.minTempC.toInt())
+            feelsLikeText.text = itemView.context.getString(R.string.feels_like, forecastWeatherResponse.currentWeather.feelsLikeC.toInt())
+        }
+    }
+}
+
+class WeatherCardListAdapter(
+    private val cards: List<ForecastWeatherResponse>
+): RecyclerView.Adapter<WeatherCardViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherCardViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = WeatherCardItemBinding.inflate(inflater,parent,false)
+        return WeatherCardViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: WeatherCardViewHolder, position: Int) {
+        val card: ForecastWeatherResponse = cards[position]
+        holder.bind(card)
+    }
+
+    override fun getItemCount(): Int = cards.size
+}
