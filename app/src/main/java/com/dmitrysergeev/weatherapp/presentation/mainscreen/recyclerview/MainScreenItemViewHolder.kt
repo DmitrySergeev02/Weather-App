8628e88ba7f299bb7550c5defb173051c261e985
DmitrySergeev02 <justbringit16@yandex.ru>
2025-04-11 01:57:41 +0300

    Добавлено Precipitation Main Screen Item с помощью databindig и include

--- app/src/main/java/com/dmitrysergeev/weatherapp/presentation/mainscreen/recyclerview/MainScreenItemViewHolder.kt
+++ app/src/main/java/com/dmitrysergeev/weatherapp/presentation/mainscreen/recyclerview/MainScreenItemViewHolder.kt
@@ -1,10 +1,13 @@
 package com.dmitrysergeev.weatherapp.presentation.mainscreen.recyclerview
 
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
+import com.dmitrysergeev.weatherapp.R
 import com.dmitrysergeev.weatherapp.data.weather.model.ForecastWeatherResponse
+import com.dmitrysergeev.weatherapp.databinding.PrecipitationMainScreenItemBinding
 import com.dmitrysergeev.weatherapp.databinding.QuickMenuRecyclerViewBinding
 import com.dmitrysergeev.weatherapp.databinding.TmpScreenMenuItemBinding
 import com.dmitrysergeev.weatherapp.databinding.WeatherCardsRecyclerViewBinding
@@ -36,6 +39,22 @@ class MainScreenItemViewHolder(private val binding: ViewBinding): RecyclerView.V
             MainScreenItemListAdapter.TMP_MENU_ITEM_VIEW -> {
                 (binding as TmpScreenMenuItemBinding).apply {
                     title.text = item.title
+                }
+            }
+            MainScreenItemListAdapter.PRECIPITATION_ITEM_VIEW -> {
+                (binding as PrecipitationMainScreenItemBinding).apply {
+                    humidityItem.setTitle("Humidity")
+                    humidityItem.setIconSrc(AppCompatResources.getDrawable(root.context,R.drawable.humidity))
+                    humidityItem.value.text = "88%"
+                    rainProbabilityItem.setTitle("Rain Probability")
+                    rainProbabilityItem.setIconSrc(AppCompatResources.getDrawable(root.context,R.drawable.humidity))
+                    rainProbabilityItem.value.text = "88%"
+                    dewPointItem.setTitle("Dew Point")
+                    dewPointItem.setIconSrc(AppCompatResources.getDrawable(root.context,R.drawable.humidity))
+                    dewPointItem.value.text = "88%"
+                    cloudOverItem.setTitle("Cloud over")
+                    cloudOverItem.setIconSrc(AppCompatResources.getDrawable(root.context,R.drawable.humidity))
+                    cloudOverItem.value.text = "88%"
                 }
             }
         }
