1d9177c8b89bcba32792c91a3c4d83a6c7ed4653
Dmitry Sergeev <justbringit16@yandex.ru>
2025-04-21 16:54:23 +0300

    Добавлена окраска точки

--- app/src/main/java/com/dmitrysergeev/weatherapp/presentation/mainscreen/MyProgressBar.kt
+++ app/src/main/java/com/dmitrysergeev/weatherapp/presentation/mainscreen/MyProgressBar.kt
@@ -13,12 +13,55 @@ import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.util.Log
 import android.widget.ProgressBar
+import androidx.collection.arrayMapOf
 import androidx.core.graphics.toColorInt
 
 class MyProgressBar(
     context: Context,
     attrs: AttributeSet
 ): ProgressBar(context, attrs) {
+
+    private val colorMap: Map<Float,Int> = arrayMapOf(
+         0f to "#28FF30".toColorInt(),
+         0.22f to "#F0FF47".toColorInt(),
+         0.5f to "#FFCC47".toColorInt(),
+         0.75f to "#FF1A43".toColorInt(),
+         0.93f to "#B519FF".toColorInt(),
+         1f to "#76000E".toColorInt()
+    )
+
+    private fun getColor(value: Float): Int {
+        for (i in 0 until colorMap.size){
+            if (value>=colorMap.keys.toFloatArray()[i] && value<colorMap.keys.toFloatArray()[i+1]){
+                val startColor = colorMap.values.toIntArray()[i]
+                val endColor = colorMap.values.toIntArray()[i+1]
+                val sR = Color.red(startColor)
+                val sG = Color.green(startColor)
+                val sB = Color.blue(startColor)
+                val eR = Color.red(endColor)
+                val eG = Color.green(endColor)
+                val eB = Color.blue(endColor)
+                val hsvStart = FloatArray(3)
+                val hsvEnd = FloatArray(3)
+                Color.RGBToHSV(sR,sG,sB,hsvStart)
+                Color.RGBToHSV(eR,eG,eB,hsvEnd)
+                val t = (value - colorMap.keys.toFloatArray()[i])/(colorMap.keys.toFloatArray()[i+1]-colorMap.keys.toFloatArray()[i])
+                val finalColor = Color.HSVToColor(
+                    255, floatArrayOf(
+                        linear(hsvStart[0],hsvEnd[0],t),
+                        linear(hsvStart[1],hsvEnd[1],t),
+                        linear(hsvStart[2],hsvEnd[2],t)
+                    )
+                )
+                return finalColor
+            }
+        }
+        return colorMap.values.last()
+    }
+
+    private fun linear(start: Float,end: Float, t:Float): Float{
+        return start*(1-t)+end*t
+    }
 
     init {
         val gradientDrawable = object : Drawable() {
@@ -27,33 +70,17 @@ class MyProgressBar(
             override fun draw(canvas: Canvas) {
                 val shader = LinearGradient(
                     0f,0f,bounds.width().toFloat(),0f,
-                    intArrayOf(
-                        "#28FF30".toColorInt(),
-                        "#F0FF47".toColorInt(),
-                        "#FFCC47".toColorInt(),
-                        "#FF1A43".toColorInt(),
-                        "#B519FF".toColorInt(),
-                        "#76000E".toColorInt()
-                    ),
-                    floatArrayOf(
-                        0f,
-                        0.22f,
-                        0.5f,
-                        0.75f,
-                        0.93f,
-                        1f
-                    ),
+                    colorMap.values.toIntArray(),
+                    colorMap.keys.toFloatArray(),
                     Shader.TileMode.CLAMP
                 )
                 paint.shader = shader
-                Log.d(TAG, bounds.toString())
                 val newBounds = RectF(
                     bounds.left.toFloat(),
                     bounds.height()/2-1*context.resources.displayMetrics.density,
                     bounds.right.toFloat(),
                     bounds.height()/2+1*context.resources.displayMetrics.density
                 )
-//                canvas.drawRect(newBounds, paint)
                 canvas.drawRoundRect(newBounds,10*context.resources.displayMetrics.density,10*context.resources.displayMetrics.density, paint)
 
                 // DebugMode
@@ -82,6 +109,7 @@ class MyProgressBar(
         style = Paint.Style.FILL
         color = Color.BLACK
     }
+    
     private val strokePaint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         color = Color.WHITE
@@ -89,7 +117,10 @@ class MyProgressBar(
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        val cX = progress.toFloat()/max*width
+        val relativeProgress = progress.toFloat()/max
+        val cX = relativeProgress*width
+        val color = getColor(relativeProgress)
+        circlePaint.color = color
         canvas.drawCircle(cX, height/2f, 3*context.resources.displayMetrics.density, strokePaint)
         canvas.drawCircle(cX, height/2f, 2*context.resources.displayMetrics.density, circlePaint)
     }
